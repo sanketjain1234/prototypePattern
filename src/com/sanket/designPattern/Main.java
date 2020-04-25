5d90662789869c5bc8edf91d5b091c0a3586cf83
sanket <sanketjain1234@gmail.com>
2020-04-25 18:19:11 +0530

    added main method

---
+++ src/com/sanket/designPattern/Main.java
@@ -0,0 +1,22 @@
+package com.sanket.designPattern;
+
+import com.sanket.designPattern.Factory.PrototypeFactory;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            String moviePrototype = PrototypeFactory.getInstance(PrototypeFactory.ModelType.MOVIE).toString();
+
+            System.out.println(moviePrototype);
+
+            String albumPrototype  = PrototypeFactory.getInstance(PrototypeFactory.ModelType.ALBUM).toString();
+            System.out.println(albumPrototype);
+
+            String showPrototype  = PrototypeFactory.getInstance(PrototypeFactory.ModelType.SHOW).toString();
+            System.out.println(showPrototype);
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
+    }
+}
